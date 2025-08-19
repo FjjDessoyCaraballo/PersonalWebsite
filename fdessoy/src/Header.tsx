9f7cac321756de8650ede4677a8a4ad191f3f52a
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-08-19 11:51:02 +0300

    investiganting issue with tailwind

--- fdessoy/src/Header.tsx
+++ fdessoy/src/Header.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export const Header: React.FC = () => {
 	return(
 		<header className="shadow-sm border-b text-3xl">
-			<div className="max-w-6xl mx-auto px-1 py-2 font-vt323">
+			<div className="max-w-6xl mx-auto px-1 py-2 bg-red-500 font-vt323">
 				Felipe Dessoy Caraballo
 			</div>
 		</header>
