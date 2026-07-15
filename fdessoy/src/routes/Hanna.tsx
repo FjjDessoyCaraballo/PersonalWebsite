65c2e522e3a72f15b6ba1e383f1505dde8d72063
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-07-15 19:47:59 +0300

    feature: component for new route

---
+++ fdessoy/src/routes/Hanna.tsx
@@ -0,0 +1,9 @@
+export default function Knitting() {
+  return (
+    <iframe
+      src="/hanna.html"
+      title="Colorwork Chart Studio"
+      style={{ width: "100%", height: "100vh", border: "none", display: "block" }}
+    />
+  );
+}
